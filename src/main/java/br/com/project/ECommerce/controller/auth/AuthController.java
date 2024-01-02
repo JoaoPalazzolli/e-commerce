72ad0c28d8e351377cd1b513e7dc7e13faca56df
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-01-01 23:00:43 -0300

    Criado métodos para registrar uma conta juridica e uma fisica

--- src/main/java/br/com/project/ECommerce/controller/auth/AuthController.java
+++ src/main/java/br/com/project/ECommerce/controller/auth/AuthController.java
@@ -1,6 +1,7 @@
 package br.com.project.ECommerce.controller.auth;
 
-import br.com.project.ECommerce.dto.auth.RegisterRequestDTO;
+import br.com.project.ECommerce.dto.auth.RegisterPessoaFisicaDTO;
+import br.com.project.ECommerce.dto.auth.RegisterPessoaJuridicaDTO;
 import br.com.project.ECommerce.service.auth.AuthServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,19 @@ public class AuthController {
     @Autowired
     private AuthServices authServices;
 
-    @PostMapping(value = "/signup")
-    public ResponseEntity register(@RequestBody RegisterRequestDTO data){
-        return authServices.signup(data);
+    @PostMapping(value = "/signup/pessoafisica")
+    public ResponseEntity<?> signup(@RequestBody RegisterPessoaFisicaDTO data){
+        return authServices.signupPessoaFisica(data);
+    }
+
+    @PostMapping(value = "/signup/pessoajuridica")
+    public ResponseEntity<?> signup(@RequestBody RegisterPessoaJuridicaDTO data){
+        return authServices.signupPessoaJuridica(data);
+    }
+
+    @GetMapping(value = "/signup/pessoafisica")
+    public ResponseEntity<?> teste(){
+        return authServices.teste();
     }
 
 }
